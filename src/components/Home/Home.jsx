e7738bcb66c70c505f9aa92df492703314bd064a
Akash Bhat <aabhat@wisc.edu>
2024-10-10 22:37:44 -0500

    Added the home page and the about section

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Card, Container } from 'react-bootstrap'
+import './home.css'
+import akash from '../../assets/akash1.jpeg'
+import { FaGithub, FaLinkedin, FaDiscord,FaEnvelope } from 'react-icons/fa'; 
+
+
+function Home() {
+  return (
+    <Container className="hello-container">
+        <Card className='bg-transparent border-0'>
+            <Card.Body >
+                <h4>Hello, my name is</h4>
+                <h1>Akash Bhat.</h1>
+                <p> I am Akash Bhat, a Computer Science and Data Science professional based in Madison, WI. With a solid foundation in programming and data analysis, I have developed practical solutions through internships and personal projects. My experience includes enhancing customer feedback systems with natural language processing at Numberz.ai and mentoring peers in advanced programming concepts. I am passionate about creating innovative applications that leverage technology to solve real-world problems, and I continuously seek opportunities to expand my skills in software development and data science. </p>
+                <div className="social-icons">
+                    <a href="https://github.com/quentesia" target="_blank" rel="noopener noreferrer">
+                        <FaGithub size={25} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/akashbhat1" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin size={25} />
+                    </a>
+                    <a href="https://discordapp.com/users/zoro9997" target="_blank" rel="noopener noreferrer">
+                        <FaDiscord size={25} />
+                    </a>
+                    <a href="mailto:aabhat@wisc.edu">
+                        <FaEnvelope size={25} />
+                    </a>
+                </div>
+            </Card.Body>
+        </Card>
+        <img src={akash} alt="Akash" className="akash-image" />
+    </Container>
+  )
+}
+
+export default Home
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -5,12 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary fixed-top justify-content-end"> 
-      <Container>
+      <Container className='ms-auto'>
         <Navbar.Brand href="#home">Akash</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto"> 
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#hello-container">Home</Nav.Link>
             <Nav.Link href="#link">About</Nav.Link>
             <Nav.Link href="#skills">Skills</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
